277620cbf37bf448d97505c299950f820c36a6ca
Dima Komisarov <komissarov854@gmail.com>
2025-03-15 13:18:02 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('li > ul');
+
+items.forEach((ul) => {
+  const li = ul.parentElement;
+  const span = document.createElement('span');
+
+  span.textContent = li.firstChild.textContent.trim();
+  li.firstChild.textContent = '';
+  li.prepend(span);
+
+  span.addEventListener('click', () => {
+    const nestedUl = li.querySelector('ul');
+
+    if (nestedUl) {
+      nestedUl.style.display =
+        nestedUl.style.display === 'none' ? 'block' : 'none';
+    }
+  });
+});
